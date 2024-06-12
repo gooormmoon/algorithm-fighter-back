0fb1610510c38308364a08911a7155d1b6e6549b
sshinylee <sunshinylee0@gmail.com>
2024-06-12 15:14:05 +0900

    feat: Update member entity

--- algofi-core/src/main/java/gooroommoon/algofi_core/auth/member/Member.java
+++ algofi-core/src/main/java/gooroommoon/algofi_core/auth/member/Member.java
@@ -1,10 +1,10 @@
 package gooroommoon.algofi_core.auth.member;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
 
@@ -12,30 +12,51 @@ import java.time.LocalDateTime;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
+@Table(uniqueConstraints = { @UniqueConstraint(name = "UniqueLoginId", columnNames = "loginId") })
 public class Member {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "MEMBER_ID")
     private Long id;
 
+    @NotNull
+    @Setter
     private String name;
 
+    @NotNull
+    @Setter
     private String nickname;
 
+    @NotNull
+    @Setter
     private String loginId;
 
+    @NotNull
+    @Setter
     private String password;
 
     @Enumerated(EnumType.STRING)
     private MemberRole role;
 
+    @Setter
     private String profileImageUrl;
 
+    @Setter
     private String description;
 
-    @CreatedDate
+    @Setter
+    private String alert;
+
+    @CreatedDate @NotNull
+    @Setter
     private LocalDateTime createdDate;
 
+    @NotNull
+    @Setter
     private LocalDateTime loginDate;
+
+    @LastModifiedDate @NotNull
+    private LocalDateTime updatedDate;
 
 }
