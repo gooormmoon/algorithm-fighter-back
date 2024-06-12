0e1e6cfd3e16167bb01463eb4b1dd995b2a1a130
Berygna <berygna@gmail.com>
2024-06-12 16:03:53 +0900

    GRTEAM-24 Compile Project Setting

---
+++ algofi-compile/src/main/java/gooroommoon/algofi_compile/AlgofiCompileApplication.java
@@ -0,0 +1,13 @@
+package gooroommoon.algofi_compile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgofiCompileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgofiCompileApplication.class, args);
+	}
+
+}
