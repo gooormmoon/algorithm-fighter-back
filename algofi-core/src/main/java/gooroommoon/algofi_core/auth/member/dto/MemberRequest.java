b0d3da6de6adbcd1537bd55f8e65c08ca10d533a
sshinylee <sunshinylee0@gmail.com>
2024-06-12 16:30:05 +0900

    feat: Add DTO for register

--- algofi-core/src/main/java/gooroommoon/algofi_core/auth/member/dto/MemberRequest.java
+++ algofi-core/src/main/java/gooroommoon/algofi_core/auth/member/dto/MemberRequest.java
@@ -2,6 +2,7 @@ package gooroommoon.algofi_core.auth.member.dto;
 
 import gooroommoon.algofi_core.auth.member.Member;
 import gooroommoon.algofi_core.auth.member.MemberRole;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.*;
 
 @Getter
@@ -15,4 +16,17 @@ public class MemberRequest {
     private String nickname;
     private String profileImageUrl;
     private String description;
+
+    public static class RegisterRequest {
+        @NotEmpty(message = "아이디는 필수 항목입니다.")
+        private String id;
+        @NotEmpty(message = "비밀번호는 필수 항목입니다.")
+        private String password;
+        @NotEmpty(message = "이름은 필수 항목입니다.")
+        private String name;
+
+        private String nickname;
+        private String profileImageUrl;
+        private String description;
+    }
 }
