c17568ca87a95af24b59cbfb3c35f0b0564a8aae
sshinylee <sunshinylee0@gmail.com>
2024-06-12 15:17:07 +0900

    feat: Add SecurityConfig and set filters, handlers

---
+++ algofi-core/src/main/java/gooroommoon/algofi_core/auth/filter/JwtFilter.java
@@ -0,0 +1,31 @@
+package gooroommoon.algofi_core.auth.filter;
+
+import gooroommoon.algofi_core.auth.util.JwtUtil;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+        if(token != null && !jwtUtil.isExpired(token)) {
+            Authentication authentication = jwtUtil.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request, response);
+    }
+}
