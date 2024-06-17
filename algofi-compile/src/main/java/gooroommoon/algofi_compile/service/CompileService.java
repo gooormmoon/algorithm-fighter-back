6b494cd2e598c4715d99860e42d732fe7752eefd
cherry-go-round <hongseokhodev@gmail.com>
2024-06-17 12:05:05 +0900

    refactor: if 문 내 메소드 분리(CompileService.runCode())

--- algofi-compile/src/main/java/gooroommoon/algofi_compile/service/CompileService.java
+++ algofi-compile/src/main/java/gooroommoon/algofi_compile/service/CompileService.java
@@ -17,39 +17,13 @@ public class CompileService {
         ProcessBuilder builder = new ProcessBuilder();
 
         if (language.equalsIgnoreCase("c")) {
-            Path cFilePath = Paths.get("temp." + language.toLowerCase());
-            Files.write(cFilePath, code.getBytes());
-
-            builder.command("sh", "-c", "gcc " + cFilePath.toString() + " -o temp && ./temp"); //linux 버전
-            //builder.command("cmd.exe", "/c", "gcc " + cFilePath.toString() + " -o temp && temp"); //windows 버전
+            commandCFile(code, language, builder);
         } else if (language.equalsIgnoreCase("java")) {
-            int classIndex = code.indexOf("public class ");
-            if (classIndex != -1) {
-                int startIndex = classIndex + "public class ".length();
-                int endIndex = code.indexOf(" ", startIndex);
-                if (endIndex != -1) {
-                    fileName = code.substring(startIndex, endIndex);
-                }
-            }
-
-            Path javaFilePath = Paths.get(fileName + "." + language.toLowerCase());
-            Files.write(javaFilePath, code.getBytes());
-
-            String className = javaFilePath.toString().replace(".java", "");
-            //builder.command("cmd.exe", "/c", "javac " + javaFilePath.toString() + " && java " + className); //windows 버전
-            builder.command("sh", "-c", "javac " + javaFilePath.toString() + " && java " + className); //linux 버전
+            commandJavaFile(code, language, fileName, builder);
         } else if (language.equalsIgnoreCase("javascript")) {
-            Path jsFilePath = Paths.get("temp." + language.toLowerCase());
-            Files.write(jsFilePath, code.getBytes());
-
-            //builder.command("cmd.exe", "/c", "node " + jsFilePath.toString()); //windows 버전
-            builder.command("sh", "-c", "node " + jsFilePath.toString()); //linux 버전
+            commandJavaScriptFile(code, language, builder);
         } else if (language.equalsIgnoreCase("python")) {
-            Path pyFilePath = Paths.get("temp." + language.toLowerCase());
-            Files.write(pyFilePath, code.getBytes());
-
-            //builder.command("cmd.exe", "/c", "python " + pyFilePath.toString()); //windows 버전
-            builder.command("sh", "-c", "python3 " + pyFilePath.toString()); //linux 버전
+            commandPythonFile(code, language, builder);
         } else {
             throw new IllegalArgumentException("Unsupported language: " + language);
         }
@@ -68,5 +42,51 @@ public class CompileService {
             throw new RuntimeException("Execution failed with exit code " + exitCode);
         }
         return output.toString();
+    }
+
+    private void commandCFile(String code, String language, ProcessBuilder builder) throws IOException {
+        Path cFilePath = getTempFilePath(language);
+        Files.write(cFilePath, code.getBytes());
+
+        builder.command("sh", "-c", "gcc " + cFilePath.toString() + " -o temp && ./temp"); //linux 버전
+        //builder.command("cmd.exe", "/c", "gcc " + cFilePath.toString() + " -o temp && temp"); //windows 버전
+    }
+
+    private void commandJavaFile(String code, String language, String fileName, ProcessBuilder builder) throws IOException {
+        int classIndex = code.indexOf("public class ");
+        if (classIndex != -1) {
+            int startIndex = classIndex + "public class ".length();
+            int endIndex = code.indexOf(" ", startIndex);
+            if (endIndex != -1) {
+                fileName = code.substring(startIndex, endIndex);
+            }
+        }
+
+        Path javaFilePath = Paths.get(fileName + "." + language.toLowerCase());
+        Files.write(javaFilePath, code.getBytes());
+
+        String className = javaFilePath.toString().replace(".java", "");
+        //builder.command("cmd.exe", "/c", "javac " + javaFilePath.toString() + " && java " + className); //windows 버전
+        builder.command("sh", "-c", "javac " + javaFilePath.toString() + " && java " + className); //linux 버전
+    }
+
+    private void commandJavaScriptFile(String code, String language, ProcessBuilder builder) throws IOException {
+        Path jsFilePath = getTempFilePath(language);
+        Files.write(jsFilePath, code.getBytes());
+
+        //builder.command("cmd.exe", "/c", "node " + jsFilePath.toString()); //windows 버전
+        builder.command("sh", "-c", "node " + jsFilePath.toString()); //linux 버전
+    }
+
+    private void commandPythonFile(String code, String language, ProcessBuilder builder) throws IOException {
+        Path pyFilePath = getTempFilePath(language);
+        Files.write(pyFilePath, code.getBytes());
+
+        //builder.command("cmd.exe", "/c", "python " + pyFilePath.toString()); //windows 버전
+        builder.command("sh", "-c", "python3 " + pyFilePath.toString()); //linux 버전
+    }
+
+    private Path getTempFilePath(String language) {
+        return Paths.get("temp." + language.toLowerCase());
     }
 }
