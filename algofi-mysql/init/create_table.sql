02ce4507f339a3089c4b65cd2d0cb3068b04bc55
immyeong <wpfls6670@naver.com>
2024-06-17 16:37:10 +0900

    feat: message table type column 추가

--- algofi-mysql/init/create_table.sql
+++ algofi-mysql/init/create_table.sql
@@ -1,98 +1,104 @@
--- create member table
-CREATE TABLE IF NOT EXISTS member (
-                                      id INT AUTO_INCREMENT PRIMARY KEY,
-                                      name VARCHAR(50) NOT NULL,
-                                      nickname VARCHAR(50) NOT NULL,
-                                      login_id VARCHAR(50) NOT NULL,
-                                      password VARCHAR(50) NOT NULL,
-                                      role VARCHAR(50) NOT NULL,
-                                      profile_image_url VARCHAR(255),
-                                      description TEXT,
-                                      alert VARCHAR(50)
+/* member table 생성 */
+CREATE TABLE member (
+                        member_id INT(11) AUTO_INCREMENT PRIMARY KEY,
+                        name VARCHAR(10) NOT NULL,
+                        nickname VARCHAR(30) NOT NULL,
+                        login_id VARCHAR(100) UNIQUE NOT NULL,
+                        password VARCHAR(100) NOT NULL,
+                        role VARCHAR(255),
+                        profile_image_url VARCHAR(255),
+                        description VARCHAR(255),
+                        alert VARCHAR(255),
+                        login_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                        created_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                        updated_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
--- create filestorage table
-CREATE TABLE IF NOT EXISTS filestorage (
-                                           id INT AUTO_INCREMENT PRIMARY KEY,
-                                           name VARCHAR(50) NOT NULL,
-                                           member_id INT NOT NULL,
-                                           FOREIGN KEY (member_id) REFERENCES member(id)
+/* filestorage table 생성 */
+CREATE TABLE filestorage (
+                             file_storage_id INT(11) AUTO_INCREMENT PRIMARY KEY,
+                             name VARCHAR(100) NOT NULL,
+                             member_id INT NOT NULL,
+                             created_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                             updated_time TIMESTAMP,
+                             FOREIGN KEY(member_id) REFERENCES member(member_id)
 );
 
--- create file table
-CREATE TABLE IF NOT EXISTS file (
-                                    id INT AUTO_INCREMENT PRIMARY KEY,
-                                    content TEXT NOT NULL,
-                                    name VARCHAR(255) NOT NULL,
-                                    type VARCHAR(50) NOT NULL,
-                                    path VARCHAR(255) NOT NULL,
-                                    language VARCHAR(50) NOT NULL,
-                                    file_storage_id INT NOT NULL,
-                                    parent_id INT,
-                                    FOREIGN KEY (file_storage_id) REFERENCES filestorage(id),
-                                    FOREIGN KEY (parent_id) REFERENCES file(id) ON DELETE CASCADE
+/* file table 생성 */
+CREATE TABLE file (
+                      file_id INT(11) AUTO_INCREMENT PRIMARY KEY,
+                      content TEXT,
+                      name VARCHAR(255),
+                      type VARCHAR(255),
+                      path VARCHAR(255),
+                      language VARCHAR(20),
+                      file_storage_id INT NOT NULL,
+                      parent_id INT NULL,
+                      FOREIGN KEY(file_storage_id) REFERENCES filestorage(file_storage_id),
+                      FOREIGN KEY(parent_id) REFERENCES file(file_id) ON DELETE CASCADE
 );
 
--- create algorithmproblem table
-CREATE TABLE IF NOT EXISTS algorithmproblem (
-                                                id INT AUTO_INCREMENT PRIMARY KEY,
-                                                title VARCHAR(255) NOT NULL,
-                                                level INT NOT NULL,
-                                                content TEXT NOT NULL,
-                                                recommend_time INT NOT NULL
+/* algorithmproblem table 생성 */
+CREATE TABLE algorithmproblem (
+                                  algorithm_problem_id INT(11) AUTO_INCREMENT PRIMARY KEY,
+                                  title VARCHAR(100) NOT NULL,
+                                  level VARCHAR(10) NOT NULL,
+                                  content TEXT,
+                                  recommend_time INT
 );
 
--- create chatroom table
-CREATE TABLE IF NOT EXISTS chatroom (
-                                        id INT AUTO_INCREMENT PRIMARY KEY,
-                                        chatroom_name VARCHAR(255) NOT NULL
+/* chatroom table 생성 */
+CREATE TABLE chatroom (
+                          chatroom_id INT(11) AUTO_INCREMENT PRIMARY KEY,
+                          chatroom_name VARCHAR(100)
 );
 
--- create message table
-CREATE TABLE IF NOT EXISTS message (
-                                       id INT AUTO_INCREMENT PRIMARY KEY,
-                                       content TEXT NOT NULL,
-                                       sender_id INT NOT NULL,
-                                       chatroom_id INT NOT NULL,
-                                       FOREIGN KEY (sender_id) REFERENCES member(id),
-                                       FOREIGN KEY (chatroom_id) REFERENCES chatroom(id)
+/* message table 생성 */
+CREATE TABLE message (
+                         message_id INT(11) AUTO_INCREMENT PRIMARY KEY,
+                         content TEXT,
+                         type VARCHAR(10),
+                         created_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                         sender_id INT NULL,
+                         chatroom_id INT NULL,
+                         FOREIGN KEY(sender_id) REFERENCES member(member_id),
+                         FOREIGN KEY(chatroom_id) REFERENCES chatroom(chatroom_id)
 );
 
--- create member_chatroom table
-CREATE TABLE IF NOT EXISTS member_chatroom (
-                                               chatroom_id INT NOT NULL,
-                                               member_id INT NOT NULL,
-                                               PRIMARY KEY (chatroom_id, member_id),
-                                               FOREIGN KEY (chatroom_id) REFERENCES chatroom(id),
-                                               FOREIGN KEY (member_id) REFERENCES member(id)
+/* member_chatroom table 생성 */
+CREATE TABLE member_chatroom (
+                                 chatroom_id INT NULL,
+                                 member_id INT NULL,
+                                 FOREIGN KEY(chatroom_id) REFERENCES chatroom(chatroom_id),
+                                 FOREIGN KEY(member_id) REFERENCES  member(member_id)
 );
 
--- create gameresult table
-CREATE TABLE IF NOT EXISTS gameresult (
-                                          id INT AUTO_INCREMENT PRIMARY KEY,
-                                          running_time VARCHAR(50) NOT NULL,
-                                          member_code_content TEXT NOT NULL,
-                                          other_member_code_content TEXT NOT NULL,
-                                          algorithm_problem_id INT NOT NULL,
-                                          chatroom_id INT NOT NULL,
-                                          FOREIGN KEY (algorithm_problem_id) REFERENCES algorithmproblem(id),
-                                          FOREIGN KEY (chatroom_id) REFERENCES chatroom(id)
+/* gameresult table 생성 */
+CREATE TABLE gameresult (
+                            game_result_id INT(11) AUTO_INCREMENT PRIMARY KEY,
+                            running_time VARCHAR(100),
+                            member_code_content VARCHAR(255),
+                            other_member_code_content VARCHAR(255),
+                            created_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                            algorithm_problem_id INT NOT NULL,
+                            chatroom_id INT NOT NULL,
+                            FOREIGN KEY (algorithm_problem_id) REFERENCES algorithmproblem(algorithm_problem_id),
+                            FOREIGN KEY (chatroom_id) REFERENCES chatroom(chatroom_id)
 );
 
--- create member_gameresult table
-CREATE TABLE IF NOT EXISTS member_gameresult (
-                                                 game_result_id INT NOT NULL,
-                                                 member_id INT NOT NULL,
-                                                 PRIMARY KEY (game_result_id, member_id),
-                                                 FOREIGN KEY (game_result_id) REFERENCES gameresult(id),
-                                                 FOREIGN KEY (member_id) REFERENCES member(id)
+/* member_gameresult table 생성 */
+CREATE TABLE member_gameresult (
+                                   game_result_id INT NOT NULL,
+                                   member_id INT NOT NULL,
+                                   FOREIGN KEY(game_result_id) REFERENCES gameresult(game_result_id),
+                                   FOREIGN KEY(member_id) REFERENCES member(member_id)
 );
 
--- create testcase table
-CREATE TABLE IF NOT EXISTS testcase (
-                                        id INT AUTO_INCREMENT PRIMARY KEY,
-                                        test_input TEXT NOT NULL,
-                                        test_output TEXT NOT NULL,
-                                        algorithm_problem_id INT NOT NULL,
-                                        FOREIGN KEY (algorithm_problem_id) REFERENCES algorithmproblem(id)
+/* testcase table 생성 */
+CREATE TABLE testcase (
+                          test_case_id INT(11) AUTO_INCREMENT PRIMARY KEY,
+                          test_input VARCHAR(500),
+                          test_output VARCHAR(500),
+                          algorithm_problem_id INT NOT NULL,
+                          FOREIGN KEY(algorithm_problem_id) REFERENCES algorithmproblem(algorithm_problem_id)
 );
