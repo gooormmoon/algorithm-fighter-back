a76f5254ceeb3cc27ede691842705f1e94da089f
Berygna <berygna@gmail.com>
2024-06-17 17:35:26 +0900

    fix: JWT expired issue

---
+++ algofi-mysql/select-query.sql
@@ -0,0 +1,12 @@
+show tables;
+
+select * from algorithmproblem;
+select * from chatroom;
+select * from file;
+select * from filestorage;
+select * from gameresult;
+select * from member;
+select * from member_chatroom;
+select * from member_gameresult;
+select * from message;
+select * from testcase;
