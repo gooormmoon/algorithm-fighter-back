ebed3f0dceb00a95ef98dd19a37f6766f77f11e1
cherry-go-round <hongseokhodev@gmail.com>
2024-06-18 11:15:55 +0900

    refactor: NoArgsConstructor 제거, 필드 final 키워드 추가 (CodeExecutionRequest)

--- algofi-compile/src/main/java/gooroommoon/algofi_compile/dto/CodeExecutionRequest.java
+++ algofi-compile/src/main/java/gooroommoon/algofi_compile/dto/CodeExecutionRequest.java
@@ -1,18 +1,15 @@
 package gooroommoon.algofi_compile.dto;
 
-import lombok.AccessLevel;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @ToString
 public class CodeExecutionRequest {
-    private String code;
-    private String language;
-    private String input;
-    private String expected;
+    private final String code;
+    private final String language;
+    private final String input;
+    private final String expected;
 
     public CodeExecutionRequest(String code, String language, String input, String expected) {
         this.code = code;
