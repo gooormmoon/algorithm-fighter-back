5816d83e369959786b6ce2cef5ff7d889d868ba9
hanni66 <haeunkim0807@gmail.com>
2024-06-19 00:25:18 +0900

    fix: config 수정

--- algofi-core/src/main/java/gooroommoon/algofi_core/config/WebSocketConfig.java
+++ algofi-core/src/main/java/gooroommoon/algofi_core/config/WebSocketConfig.java
@@ -12,8 +12,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/chat").setAllowedOrigins("*");
-//                .withSockJS();
+        registry.addEndpoint("/chat").setAllowedOrigins("*").withSockJS();
     }
 
     @Override
