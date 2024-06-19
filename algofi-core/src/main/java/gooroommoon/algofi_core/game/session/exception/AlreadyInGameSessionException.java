04015a73d107922789dc86c2801da66ea313d605
sshinylee <sunshinylee0@gmail.com>
2024-06-19 15:38:05 +0900

    feat: declare exceptions

---
+++ algofi-core/src/main/java/gooroommoon/algofi_core/game/session/exception/AlreadyInGameSessionException.java
@@ -0,0 +1,7 @@
+package gooroommoon.algofi_core.game.session.exception;
+
+public class AlreadyInGameSessionException extends RuntimeException {
+    public AlreadyInGameSessionException(String message) {
+        super(message);
+    }
+}
---
+++ algofi-core/src/main/java/gooroommoon/algofi_core/game/session/exception/GameSessionNotFoundException.java
@@ -0,0 +1,7 @@
+package gooroommoon.algofi_core.game.session.exception;
+
+public class GameSessionNotFoundException extends RuntimeException{
+    public GameSessionNotFoundException(String message) {
+        super(message);
+    }
+}
