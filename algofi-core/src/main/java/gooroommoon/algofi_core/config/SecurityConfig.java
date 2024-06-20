1e460bf67c219b97241c2a25facae4325cf48aa5
Berygna <berygna@gmail.com>
2024-06-20 15:19:44 +0900

    apply PR(~36)-develop branch

--- algofi-core/src/main/java/gooroommoon/algofi_core/config/SecurityConfig.java
+++ algofi-core/src/main/java/gooroommoon/algofi_core/config/SecurityConfig.java
@@ -37,7 +37,9 @@ public class SecurityConfig {
                 .authorizeHttpRequests(requests ->
                         requests
                                 .requestMatchers("/api/member/register", "/api/member/login", "/api/members/*").permitAll()
-                                .anyRequest().permitAll())
+                                .requestMatchers("/topic/room/*").permitAll()
+                                .requestMatchers("/game", "/chat").permitAll()
+                                .anyRequest().authenticated())
                 .addFilterBefore(new JwtFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
 
         http
