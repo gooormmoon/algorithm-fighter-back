c4d11a5cbb9fec14eb7a1fac313c162e17355703
immyeong <wpfls6670@naver.com>
2024-06-20 16:16:12 +0900

    feat: NotFoundException추가

---
+++ algofi-core/src/main/java/gooroommoon/algofi_core/algorithmproblem/exception/AlgorithmProblemNotFoundException.java
@@ -0,0 +1,7 @@
+package gooroommoon.algofi_core.algorithmproblem.exception;
+
+public class AlgorithmProblemNotFoundException extends RuntimeException{
+    public AlgorithmProblemNotFoundException(String message) {
+        super(message);
+    }
+}
