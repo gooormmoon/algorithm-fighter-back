17fcb56ec615a61a42194b7b6d66a1e3686e5719
cherry-go-round <hongseokhodev@gmail.com>
2024-06-20 12:51:00 +0900

    refactor: 파일 생성, 실행, 삭제를 각 언어 별로 분리

--- algofi-compile/src/main/java/gooroommoon/algofi_compile/service/CompileService.java
+++ algofi-compile/src/main/java/gooroommoon/algofi_compile/service/CompileService.java
@@ -2,50 +2,39 @@ package gooroommoon.algofi_compile.service;
 
 import gooroommoon.algofi_compile.dto.CodeExecutionRequest;
 import gooroommoon.algofi_compile.dto.CodeExecutionResponse;
-import gooroommoon.algofi_compile.exception.RequestException;
 import gooroommoon.algofi_compile.exception.ServerException;
+import gooroommoon.algofi_compile.service.language.CodeExecutor;
+import gooroommoon.algofi_compile.service.language.Language;
 import org.springframework.stereotype.Service;
 
 import java.io.*;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
-import java.util.concurrent.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 @Service
 public class CompileService {
     private static final long TIME_OUT_MILLIS = 10000;
 
     public CodeExecutionResponse runCode(CodeExecutionRequest request) {
-        String language = request.getLanguage();
-        String code = request.getCode();
+        Language language = Language.byName(request.getLanguage());
 
-        ProcessBuilder builder = new ProcessBuilder();
-        Path path = commandFileByLanguage(language, code, builder);
+        CodeExecutor codeExecutor = language.getCodeExecutor();
 
+        Path path = codeExecutor.makeFileFromCode(request.getCode());
+        ProcessBuilder builder = codeExecutor.executeProcessBuilder(path);
         Process process = startProcess(builder);
 
-        CompletableFuture<StringBuilder> future = CompletableFuture.supplyAsync(() -> {
-            try {
-                transferInput(request.getInput(), process);
-                return readOutput(process);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        });
+        CompletableFuture<StringBuilder> future = readAndGetOutputAsync(request, process);
 
         StringBuilder output;
         try {
             output = future.get(TIME_OUT_MILLIS, TimeUnit.MILLISECONDS);
         } catch (TimeoutException e) {
-            List<ProcessHandle> children = ProcessHandle.allProcesses()
-                    .filter(processHandle -> processHandle.parent()
-                            .map(ProcessHandle::pid)
-                            .orElse(-1L) == process.pid())
-                    .toList();
-            children.forEach(ProcessHandle::destroyForcibly);
-
+            destroyChildren(process);
             process.destroyForcibly();
             return new CodeExecutionResponse(null, JudgeResult.TIME_LIMIT_EXCEEDED.getMessage());
         } catch (RuntimeException e) {
@@ -65,23 +54,7 @@ public class CompileService {
             return new CodeExecutionResponse(null, JudgeResult.RUNTIME_ERROR.getMessage());
         }
 
-        deleteFile(path);
-        if (request.getLanguage().equalsIgnoreCase("java")) {
-            String className = path.toString().replace(".java", ".class");
-            try {
-                new ProcessBuilder("rm", "-f", className).start();
-            } catch (IOException e) {
-                throw new ServerException(e);
-            }
-        }
-
-        if (request.getLanguage().equalsIgnoreCase("c")) {
-            try {
-                new ProcessBuilder("rm", "-f", path.toString().replace(".c", "")).start();
-            } catch (IOException e) {
-                throw new ServerException(e);
-            }
-        }
+        codeExecutor.deleteFile(path);
 
         String result = output.toString();
 
@@ -92,98 +65,33 @@ public class CompileService {
         return new CodeExecutionResponse(result, JudgeResult.ACCEPTED.getMessage());
     }
 
-    private Path commandFileByLanguage(String language, String code, ProcessBuilder builder) {
-
-        if (language.equalsIgnoreCase("c")) {
-            return commandCFile(code, language, builder);
-        }
-
-        if (language.equalsIgnoreCase("java")) {
-            return commandJavaFile(code, language, builder);
-        }
-
-        if (language.equalsIgnoreCase("javascript")) {
-            return commandJavaScriptFile(code, language, builder);
-        }
-
-        if (language.equalsIgnoreCase("python")) {
-            return commandPythonFile(code, language, builder);
-        }
-
-        throw new RequestException("Unsupported language: " + language);
-    }
-
-    private Path commandCFile(String code, String language, ProcessBuilder builder) {
-        Path cFilePath = getTempFilePath(language);
+    private Process startProcess(ProcessBuilder builder) {
+        builder.redirectErrorStream(true);
         try {
-            Files.write(cFilePath, code.getBytes());
+            return builder.start();
         } catch (IOException e) {
             throw new ServerException(e);
         }
-
-        builder.command("sh", "-c", "gcc " + cFilePath + " -o temp && ./temp"); //linux 버전
-        //builder.command("cmd.exe", "/c", "gcc " + cFilePath.toString() + " -o temp && temp"); //windows 버전
-        return cFilePath;
     }
 
-    private Path commandJavaFile(String code, String language, ProcessBuilder builder) {
-        String fileName = "";
-        int classIndex = code.indexOf("public class ");
-        if (classIndex != -1) {
-            int startIndex = classIndex + "public class ".length();
-            int endIndex = code.indexOf(" ", startIndex);
-            if (endIndex != -1) {
-                fileName = code.substring(startIndex, endIndex);
+    private CompletableFuture<StringBuilder> readAndGetOutputAsync(CodeExecutionRequest request, Process process) {
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                transferInput(request.getInput(), process);
+                return readOutput(process);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
-        }
-
-        Path javaFilePath = Paths.get(fileName + "." + language.toLowerCase());
-        try {
-            Files.write(javaFilePath, code.getBytes());
-        } catch (IOException e) {
-            throw new ServerException(e);
-        }
-
-        String className = javaFilePath.toString().replace(".java", "");
-        //builder.command("cmd.exe", "/c", "javac " + javaFilePath.toString() + " && java " + className); //windows 버전
-        builder.command("sh", "-c", "javac " + javaFilePath + " && java " + className); //linux 버전
-        return javaFilePath;
+        });
     }
 
-    private Path commandJavaScriptFile(String code, String language, ProcessBuilder builder) {
-        Path jsFilePath = getTempFilePath(language);
-        try {
-            Files.write(jsFilePath, code.getBytes());
-        } catch (IOException e) {
-            throw new ServerException(e);
-        }
-
-        //builder.command("cmd.exe", "/c", "node " + jsFilePath.toString()); //windows 버전
-        builder.command("sh", "-c", "node " + jsFilePath); //linux 버전
-        return jsFilePath;
-    }
-
-    private Path commandPythonFile(String code, String language, ProcessBuilder builder) {
-        Path pyFilePath = getTempFilePath(language);
-        try {
-            Files.write(pyFilePath, code.getBytes());
-        } catch (IOException e) {
-            throw new ServerException(e);
-        }
-
-        //builder.command("cmd.exe", "/c", "python " + pyFilePath.toString()); //windows 버전
-        builder.command("sh", "-c", "python3 " + pyFilePath); //linux 버전
-        return pyFilePath;
-    }
-
-    private Path getTempFilePath(String language) {
-        if (language.equalsIgnoreCase("javascript")) {
-            return Paths.get("temp." + "js");
-        }
-        if (language.equalsIgnoreCase("python")) {
-            return Paths.get("temp." + "py");
-        }
-        return Paths.get("temp." + language.toLowerCase());
+    private void destroyChildren(Process process) {
+        List<ProcessHandle> children = ProcessHandle.allProcesses()
+                .filter(processHandle -> processHandle.parent()
+                        .map(ProcessHandle::pid)
+                        .orElse(-1L) == process.pid())
+                .toList();
+        children.forEach(ProcessHandle::destroyForcibly);
     }
 
     private void transferInput(String input, Process process) throws IOException {
@@ -192,15 +100,6 @@ public class CompileService {
             writer.write(input);
             writer.newLine();
             writer.flush();
-        }
-    }
-
-    private Process startProcess(ProcessBuilder builder) {
-        builder.redirectErrorStream(true);
-        try {
-            return builder.start();
-        } catch (IOException e) {
-            throw new ServerException(e);
         }
     }
 
@@ -215,9 +114,5 @@ public class CompileService {
         }
 
         return output;
-    }
-
-    private void deleteFile(Path path) {
-        path.toFile().deleteOnExit();
     }
 }
