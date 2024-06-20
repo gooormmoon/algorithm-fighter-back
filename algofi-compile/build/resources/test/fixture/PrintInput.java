37798c3a3b5a6927d0949af4eec8dcb7a060d49a
cherry-go-round <hongseokhodev@gmail.com>
2024-06-20 16:24:07 +0900

    chore: 필요 없는 파일들 제거

--- algofi-compile/build/resources/test/fixture/PrintInput.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-
-public class PrintInput {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println(scanner.nextLine());
-    }
-}
--- algofi-compile/build/resources/test/fixture/While.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        while (true) {
-            System.out.print("a");
-        }
-    }
-}
