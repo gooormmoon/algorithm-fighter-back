d297742d38bf114e5d12b2400ad403bc942d2af9
cherry-go-round <hongseokhodev@gmail.com>
2024-06-21 22:32:10 +0900

    refactor: @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class) 추가

--- algofi-compile/src/main/java/gooroommoon/algofi_compile/db/presentation/dto/ProblemJudgeRequest.java
+++ algofi-compile/src/main/java/gooroommoon/algofi_compile/db/presentation/dto/ProblemJudgeRequest.java
@@ -1,10 +1,13 @@
 package gooroommoon.algofi_compile.db.presentation.dto;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class ProblemJudgeRequest {
     private final Long algorithmProblemId;
     private final String language;
