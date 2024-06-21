cfa5e1eade6755af4a645a1500528053716d6418
immyeong <wpfls6670@naver.com>
2024-06-21 11:48:55 +0900

    chore: chatroom_id uuid -> 'global'

--- algofi-mysql/init/insert_data.sql
+++ algofi-mysql/init/insert_data.sql
@@ -106,7 +106,7 @@ VALUES
     ('최대점수 구하기(냅색 알고리즘)','5','이번 정보올림피아드대회에서 좋은 성적을 내기 위하여 현수는 선생님이 주신 N개의 문제를 풀려고 합니다. 각 문제는 그것을 풀었을 때 얻는 점수와 푸는데 걸리는 시간이 주어지게 됩니다. 제한시간 M안에 N개의 문제 중 최대점수를 얻을 수 있도록 해야 합니다.(해당문제는 해당시간이 걸리면 푸는 걸로 간주한다. 한 유형당 한개만 풀 수 있습니다.)',50);
 
 -- insert chatroom
-INSERT INTO chatroom(chatroom_id, chatroom_name) VALUES('2b946c7f-abd1-4aef-a440-5d7670e4db75','GLOBAL');
+INSERT INTO chatroom(chatroom_id, chatroom_name) VALUES('global','GLOBAL');
 
 -- insert testcase
 INSERT INTO testcase(test_input, test_output, algorithm_problem_id)
