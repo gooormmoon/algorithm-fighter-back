4b69b58267f192620c31de2d3384d5a939b97277
Berygna <berygna@gmail.com>
2024-06-24 17:55:53 +0900

    Core server: PR#87

--- algofi-core/src/main/java/gooroommoon/algofi_core/gameresult/GameresultRepository.java
+++ algofi-core/src/main/java/gooroommoon/algofi_core/gameresult/GameresultRepository.java
@@ -11,14 +11,18 @@ public interface GameresultRepository extends JpaRepository<Gameresult, Long> {
     /*
     member의 모든 게임 결과 조회
      */
-    @Query("select gr from Gameresult gr inner join fetch MemberGameresult mg on gr.GameresultId = mg.gameresult.GameresultId " +
-            "inner join fetch Member m on mg.member.loginId = m.loginId where m.loginId = :loginId")
-    List<Gameresult> findGameresultsByMemberId(@Param("loginId")String loginId);
+    @Query("select mg.gameresult from MemberGameresult mg " +
+            "join fetch mg.gameresult gr " +
+            "join fetch mg.member m " +
+            "where m.loginId = :loginId")
+    List<Gameresult> findGameresultsByMemberId(@Param("loginId") String loginId);
 
     /*
     member의 특정한 게임 결과 조회
      */
-    @Query("select gr from Gameresult gr inner join fetch MemberGameresult mg on gr.GameresultId = mg.gameresult.GameresultId " +
-            "inner join fetch Member m on mg.member.loginId = m.loginId where m.loginId = :loginId and gr.GameresultId = :gameresultId")
-    Gameresult findGameresultByMemberIdAndGameresultId(@Param("loginId")String loginId, @Param("gameresultId")Long gameresultId);
+    @Query("select gr from MemberGameresult mg " +
+            "join mg.gameresult gr " +
+            "join mg.member m " +
+            "where m.loginId = :loginId and gr.gameresultId = :gameresultId")
+    Gameresult findGameresultByMemberIdAndGameresultId(@Param("loginId") String loginId, @Param("gameresultId") Long gameresultId);
 }
