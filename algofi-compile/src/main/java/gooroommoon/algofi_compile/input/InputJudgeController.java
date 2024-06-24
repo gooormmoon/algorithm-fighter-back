84c1ae2d4ec1b0dd7c32ba56a9792dc5fde16127
cherry-go-round <hongseokhodev@gmail.com>
2024-06-24 12:24:00 +0900

    refactor: inputJudgeService 분리

--- algofi-compile/src/main/java/gooroommoon/algofi_compile/input/InputJudgeController.java
+++ algofi-compile/src/main/java/gooroommoon/algofi_compile/input/InputJudgeController.java
@@ -6,32 +6,20 @@ import gooroommoon.algofi_compile.judge.exception.CodeExecutionException;
 import gooroommoon.algofi_compile.judge.exception.RequestException;
 import gooroommoon.algofi_compile.judge.exception.ServerException;
 import gooroommoon.algofi_compile.judge.service.JudgeResult;
-import gooroommoon.algofi_compile.judge.service.JudgeService;
-import gooroommoon.algofi_compile.judge.service.language.CodeExecutor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.nio.file.Path;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/judge-input")
 public class InputJudgeController {
 
-    private final JudgeService judgeService;
+    private final InputJudgeService inputJudgeService;
     @PostMapping
     public ResponseEntity<CodeExecutionResponse> judgeInput(@RequestBody CodeExecutionRequest request) {
-        CodeExecutor codeExecutor = judgeService.getCodeExecutor(request.getLanguage());
-
-        Path path = codeExecutor.makeFileFromCode(request.getCode());
-
-        Process process = judgeService.executeCode(codeExecutor, path);
-
-        StringBuilder output = insertInputAndGetOutput(request.getInput(), process, codeExecutor, path);
-
-        String result = output.toString();
+        String result = inputJudgeService.judgeInput(request.getLanguage(), request.getCode(), request.getInput());
         CodeExecutionResponse response = generateResponse(request.getExpected(), result);
         return ResponseEntity.ok(response);
     }
@@ -52,15 +40,6 @@ public class InputJudgeController {
     public ResponseEntity<CodeExecutionResponse> handleCodeExecutionException(CodeExecutionException e) {
         CodeExecutionResponse response = new CodeExecutionResponse(null, e.getMessage());
         return new ResponseEntity<>(response, HttpStatus.OK);
-    }
-
-    private StringBuilder insertInputAndGetOutput(String input, Process process, CodeExecutor codeExecutor, Path path) {
-        try {
-            return judgeService.insertInputAndGetOutput(process, input);
-        } finally {
-            judgeService.destroy(process);
-            codeExecutor.deleteFile(path);
-        }
     }
 
     private CodeExecutionResponse generateResponse(String expected, String result) {
